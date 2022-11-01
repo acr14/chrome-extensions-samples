b03428c91156cb164c481ea4e3b90349d080bdaf
Andrew Rodgers <andrewrodgers@Andrews-MacBook-Air.local>
2022-11-01 16:03:16 -0700

    changed manifest to work on reddit.co changed content.js to remove content div.

--- tutorials/reading-time/scripts/content.js
+++ tutorials/reading-time/scripts/content.js
@@ -12,11 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const article = document.querySelector("article");
+// const article = document.querySelector("article");
+
+//get the class content
+const content = document.querySelector("div.content");
 
 // `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
+if (content) {
+  //remove the element
+  console.log(content);
+  content.remove();
+  console.log("removed the element");
+
   /**
    * Regular expression to find all "words" in a string.
    *
@@ -27,21 +34,21 @@ if (article) {
    * 
    * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
    */
-  const wordMatchRegExp = /[^\s]+/g;
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
+  // const wordMatchRegExp = /[^\s]+/g;
+  // const words = text.matchAll(wordMatchRegExp);
+  // // matchAll returns an iterator, convert to array to get word count
+  // const wordCount = [...words].length;
+  // const readingTime = Math.round(wordCount / 200);
+  // const badge = document.createElement("p");
+  // // Use the same styling as the publish information in an article's header
+  // badge.classList.add("color-secondary-text", "type--caption");
+  // badge.textContent = `⏱️ ${readingTime} min read`;
 
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
+  // // Support for API reference docs
+  // const heading = article.querySelector("h1");
+  // // Support for article docs with date
+  // const date = article.querySelector("time")?.parentNode;
 
-  // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
-  (date ?? heading).insertAdjacentElement("afterend", badge);
+  // // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
+  // (date ?? heading).insertAdjacentElement("afterend", badge);
 }
